064445b190f1fd4316449023d99a09ae55046066
Stuart Charters <stuart.charters@symphonicsoft.com>
2019-06-10 21:08:14 +0100

    removed getter as it was not working

--- dist/config.js
+++ dist/config.js
@@ -4,7 +4,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.G_HELPER_KEY = "G_SHEET_HELPER";
 
 exports.G_HELPER_REQUEST_REGEX = /^http:\/\/g\-sheet\-request\[\d+\]$/i;
-exports.G_HELPER_GET_REGEX = /^http:\/\/g\-sheet\-get\[\d+\]$/i;
 
 function getGSheetEnvConfig(request) {
     let config = request.getEnvironmentVariable(exports.G_HELPER_KEY);
--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const config_1 = require("./config");
 const logger_1 = require("./logger");
 const request_formatter = require("./request_formatter");
-const get_formatter = require("./get_formatter");
 
 function requestHook(context) {
     const initial_request = context.request;
     const initial_request_url = initial_request.getUrl();
-    if (!(initial_request_url.match(config_1.G_HELPER_GET_REGEX) || initial_request_url.match(config_1.G_HELPER_REQUEST_REGEX))) {
+    if (initial_request_url.match(config_1.G_HELPER_REQUEST_REGEX)) {
         return;
     }
     const sheet_option_number = Number(initial_request_url.substring(initial_request_url.indexOf("[") + 1, initial_request_url.indexOf("]")));
@@ -21,10 +20,6 @@ function requestHook(context) {
     if (sheet_config["top-left-coord"]){
         top_left_a1 = sheet_config["top-left-coord"].toLowerCase();
     }
-    var btm_right_a1 = "zz";
-    if (sheet_config["bottom-right-coord"]){
-        btm_right_a1 = sheet_config["bottom-right-coord"].toLowerCase();
-    }
     const body = initial_request.getBodyText();
     const json_body = JSON.parse(body);
     if (!json_body) {
@@ -36,8 +31,6 @@ function requestHook(context) {
         const formatted_body = request_formatter.format_body(json_body, [0,0]);
         // logger_1.log(`Formatted body: ${JSON.stringify(formatted_body)}`)
         request_formatter.format_request(sheet_id, formatted_body, initial_request, top_left_a1);
-    } else if (initial_request_url.match(config_1.G_HELPER_GET_REGEX)){
-        get_formatter.format_get(sheet_id, json_body, initial_request, top_left_a1, btm_right_a1);
     }
 }
 exports.requestHooks = [requestHook];
